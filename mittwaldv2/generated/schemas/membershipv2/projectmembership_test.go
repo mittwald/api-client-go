e44f7e67fb1217a2aa5a1d902806327dbb23e515
mittwald-machine <opensource@mittwald.de>
2025-05-08 19:02:34 +0000

    chore: update auto-generated code

--- mittwaldv2/generated/schemas/membershipv2/projectmembership_test.go
+++ mittwaldv2/generated/schemas/membershipv2/projectmembership_test.go
@@ -14,7 +14,7 @@ import (
 var _ = Describe("ProjectMembership", func() {
 	When("unmarshaling from JSON", func() {
 		It("should unmarshal", func() {
-			exampleJSON := []byte("{\"email\":\"string\",\"expiresAt\":\"2006-01-02T15:04:05Z\",\"id\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\",\"inherited\":true,\"inviteId\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\",\"memberSince\":\"2006-01-02T15:04:05Z\",\"mfa\":true,\"projectId\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\",\"role\":\"notset\",\"userId\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\"}")
+			exampleJSON := []byte("{\"avatarRef\":\"string\",\"email\":\"string\",\"expiresAt\":\"2006-01-02T15:04:05Z\",\"firstName\":\"string\",\"id\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\",\"inherited\":true,\"inviteId\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\",\"lastName\":\"string\",\"memberSince\":\"2006-01-02T15:04:05Z\",\"mfa\":true,\"projectId\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\",\"role\":\"notset\",\"userId\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\"}")
 
 			sut := membershipv2.ProjectMembership{}
 			Expect(json.Unmarshal(exampleJSON, &sut)).To(Succeed())
