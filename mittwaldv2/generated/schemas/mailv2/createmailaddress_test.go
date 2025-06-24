bc25dcea3bf39616ceac9bc8c310b5f79b719e6e
mittwald-machine <opensource@mittwald.de>
2025-06-24 19:02:34 +0000

    chore: update auto-generated code

--- mittwaldv2/generated/schemas/mailv2/createmailaddress_test.go
+++ mittwaldv2/generated/schemas/mailv2/createmailaddress_test.go
@@ -14,7 +14,7 @@ import (
 var _ = Describe("CreateMailAddress", func() {
 	When("unmarshaling from JSON", func() {
 		It("should unmarshal", func() {
-			exampleJSON := []byte("{\"address\":\"string\",\"isCatchAll\":true,\"mailbox\":{\"enableSpamProtection\":true,\"password\":\"string\",\"quotaInBytes\":2147483648}}")
+			exampleJSON := []byte("{\"address\":\"string\",\"forwardAddresses\":[\"string\"],\"isCatchAll\":true,\"mailbox\":{\"enableSpamProtection\":true,\"password\":\"string\",\"quotaInBytes\":2147483648}}")
 
 			sut := mailv2.CreateMailAddress{}
 			Expect(json.Unmarshal(exampleJSON, &sut)).To(Succeed())
