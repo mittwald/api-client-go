263f01e713057e744286e3dd7bc12b786c0c7af6
mittwald-machine <opensource@mittwald.de>
2025-09-16 19:03:19 +0000

    chore: update auto-generated code

--- mittwaldv2/generated/clients/mailclientv2/client.go
+++ mittwaldv2/generated/clients/mailclientv2/client.go
@@ -139,6 +139,11 @@ type Client interface {
 		req DeleteMailAddressRequest,
 		reqEditors ...func(req *http.Request) error,
 	) (*http.Response, error)
+	DisableMailArchive(
+		ctx context.Context,
+		req DisableMailArchiveRequest,
+		reqEditors ...func(req *http.Request) error,
+	) (*http.Response, error)
 	ListBackupsForMailAddress(
 		ctx context.Context,
 		req ListBackupsForMailAddressRequest,
@@ -177,11 +182,6 @@ type Client interface {
 	UpdateProjectMailSetting(
 		ctx context.Context,
 		req UpdateProjectMailSettingRequest,
-		reqEditors ...func(req *http.Request) error,
-	) (*http.Response, error)
-	DisableMailArchive(
-		ctx context.Context,
-		req DisableMailArchiveRequest,
 		reqEditors ...func(req *http.Request) error,
 	) (*http.Response, error)
 }
@@ -841,6 +841,30 @@ func (c *clientImpl) DeleteMailAddress(
 	return httpRes, nil
 }
 
+// Disable the mail-archive of a MailAddress.
+func (c *clientImpl) DisableMailArchive(
+	ctx context.Context,
+	req DisableMailArchiveRequest,
+	reqEditors ...func(req *http.Request) error,
+) (*http.Response, error) {
+	httpReq, err := req.BuildRequest(reqEditors...)
+	if err != nil {
+		return nil, err
+	}
+
+	httpRes, err := c.client.Do(httpReq.WithContext(ctx))
+	if err != nil {
+		return httpRes, err
+	}
+
+	if httpRes.StatusCode >= 400 {
+		err := httperr.ErrFromResponse(httpRes)
+		return httpRes, err
+	}
+
+	return httpRes, nil
+}
+
 // List backups belonging to a MailAddress.
 func (c *clientImpl) ListBackupsForMailAddress(
 	ctx context.Context,
@@ -1021,30 +1045,6 @@ func (c *clientImpl) UpdateMailAddressCatchAll(
 func (c *clientImpl) UpdateProjectMailSetting(
 	ctx context.Context,
 	req UpdateProjectMailSettingRequest,
-	reqEditors ...func(req *http.Request) error,
-) (*http.Response, error) {
-	httpReq, err := req.BuildRequest(reqEditors...)
-	if err != nil {
-		return nil, err
-	}
-
-	httpRes, err := c.client.Do(httpReq.WithContext(ctx))
-	if err != nil {
-		return httpRes, err
-	}
-
-	if httpRes.StatusCode >= 400 {
-		err := httperr.ErrFromResponse(httpRes)
-		return httpRes, err
-	}
-
-	return httpRes, nil
-}
-
-// Disable the mail-archive of a MailAddress.
-func (c *clientImpl) DisableMailArchive(
-	ctx context.Context,
-	req DisableMailArchiveRequest,
 	reqEditors ...func(req *http.Request) error,
 ) (*http.Response, error) {
 	httpReq, err := req.BuildRequest(reqEditors...)
