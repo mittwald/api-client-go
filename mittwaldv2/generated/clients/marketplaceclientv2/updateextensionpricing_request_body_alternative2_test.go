57eaeafe2321c167be2247c9ff7f1d8bb2c60e46
mittwald-machine <opensource@mittwald.de>
2025-10-15 19:03:39 +0000

    chore: update auto-generated code

--- mittwaldv2/generated/clients/marketplaceclientv2/updateextensionpricing_request_body_alternative2_test.go
+++ mittwaldv2/generated/clients/marketplaceclientv2/updateextensionpricing_request_body_alternative2_test.go
@@ -14,7 +14,7 @@ import (
 var _ = Describe("UpdateExtensionPricingRequestBodyAlternative2", func() {
 	When("unmarshaling from JSON", func() {
 		It("should unmarshal", func() {
-			exampleJSON := []byte("{\"dryRun\":true,\"pricePlan\":{\"variants\":[{\"description\":\"string\",\"descriptionChangeType\":\"FEATURE_SET_MODIFIED\",\"key\":\"string\",\"name\":\"string\",\"priceInCents\":3.14}]}}")
+			exampleJSON := []byte("{\"dryRun\":true,\"pricePlan\":{\"variants\":[{\"description\":\"string\",\"descriptionChangeType\":\"FEATURE_SET_MODIFIED\",\"isBookingStopped\":true,\"key\":\"string\",\"name\":\"string\",\"priceInCents\":3.14}]}}")
 
 			sut := marketplaceclientv2.UpdateExtensionPricingRequestBodyAlternative2{}
 			Expect(json.Unmarshal(exampleJSON, &sut)).To(Succeed())
