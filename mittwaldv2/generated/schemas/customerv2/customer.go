9c52a40251e547392b88ada1d3bc15974ea1b8a8
mittwald-machine <opensource@mittwald.de>
2025-12-11 19:04:50 +0000

    chore: update auto-generated code

--- mittwaldv2/generated/schemas/customerv2/customer.go
+++ mittwaldv2/generated/schemas/customerv2/customer.go
@@ -51,6 +51,8 @@ import (
 //        type: "boolean"
 //    "isInDefaultOfPayment":
 //        type: "boolean"
+//    "is_mail_address_invalid":
+//        type: "boolean"
 //    "levelOfUndeliverableDunningNotice":
 //        type: "string"
 //        enum:
@@ -95,6 +97,7 @@ type Customer struct {
 	IsAllowedToPlaceOrders            *bool                                      `json:"isAllowedToPlaceOrders,omitempty"`
 	IsBanned                          *bool                                      `json:"isBanned,omitempty"`
 	IsInDefaultOfPayment              *bool                                      `json:"isInDefaultOfPayment,omitempty"`
+	Is_mail_address_invalid           *bool                                      `json:"is_mail_address_invalid,omitempty"`
 	LevelOfUndeliverableDunningNotice *CustomerLevelOfUndeliverableDunningNotice `json:"levelOfUndeliverableDunningNotice,omitempty"`
 	MemberCount                       int64                                      `json:"memberCount"`
 	Name                              string                                     `json:"name"`
