3f49e7df0ac954eb58e171f7a2e3d3551e197963
mittwald-machine <opensource@mittwald.de>
2026-02-17 19:20:02 +0000

    chore: update auto-generated code

--- mittwaldv2/generated/clients/databaseclientv2/createmysqldatabase_request_body_test.go
+++ mittwaldv2/generated/clients/databaseclientv2/createmysqldatabase_request_body_test.go
@@ -14,7 +14,7 @@ import (
 var _ = Describe("CreateMysqlDatabaseRequestBody", func() {
 	When("unmarshaling from JSON", func() {
 		It("should unmarshal", func() {
-			exampleJSON := []byte("{\"database\":{\"characterSettings\":{\"characterSet\":\"utf8mb4\",\"collation\":\"utf8mb4_general_ci\"},\"description\":\"string\",\"projectId\":\"7a9d8971-09b0-4c39-8c64-546b6e1875ce\",\"version\":\"8.4\"},\"user\":{\"accessIpMask\":\"string\",\"accessLevel\":\"full\",\"externalAccess\":true,\"password\":\"string\"}}")
+			exampleJSON := []byte("{\"database\":{\"characterSettings\":{\"characterSet\":\"utf8mb4\",\"collation\":\"utf8mb4_general_ci\"},\"description\":\"string\",\"version\":\"8.4\"},\"user\":{\"accessIpMask\":\"string\",\"accessLevel\":\"full\",\"externalAccess\":true,\"password\":\"string\"}}")
 
 			sut := databaseclientv2.CreateMysqlDatabaseRequestBody{}
 			Expect(json.Unmarshal(exampleJSON, &sut)).To(Succeed())
