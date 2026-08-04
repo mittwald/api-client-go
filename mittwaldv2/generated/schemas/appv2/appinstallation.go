20e3d14451946a07e2c74e9b3e1ae2a9ab00f480
mittwald-machine <opensource@mittwald.de>
2026-08-04 19:31:25 +0000

    chore: update auto-generated code

--- mittwaldv2/generated/schemas/appv2/appinstallation.go
+++ mittwaldv2/generated/schemas/appv2/appinstallation.go
@@ -73,6 +73,8 @@ import (
 //    "shortId":
 //        type: "string"
 //        example: "a-XXXXXX"
+//    "staging":
+//        type: "boolean"
 //    "systemSoftware":
 //        type: "array"
 //        items: {"$ref": "#/components/schemas/de.mittwald.v1.app.InstalledSystemSoftware"}
@@ -128,6 +130,7 @@ type AppInstallation struct {
 	ScreenshotId       *string                    `json:"screenshotId,omitempty"`
 	ScreenshotRef      *string                    `json:"screenshotRef,omitempty"`
 	ShortId            string                     `json:"shortId"`
+	Staging            *bool                      `json:"staging,omitempty"`
 	SystemSoftware     []InstalledSystemSoftware  `json:"systemSoftware"`
 	UpdateAvailable    bool                       `json:"updateAvailable"`
 	UpdatePolicy       AppUpdatePolicy            `json:"updatePolicy"`
